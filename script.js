3d43994bc441386e21c697ca59199ba312e8f9ee
mastr777 <mstrandberg777@gmail.com>
2025-11-05 17:39:36 +0100

    Tweaking the code

--- script.js
+++ script.js
@@ -6,18 +6,17 @@ const mobDivIn = document.querySelector('.mobileFirstNavIn');
 const mobileNavClose = document.querySelector('#mobileNavClose');
 /* const width = 300;
 const maxWidth = 700; */
+const width = window.innerWidth;
 
 
-
-if (window.innerWidth < 700) {
+if (width < 700) {
 
 hamButton.addEventListener('click', hamToggle);
 function hamToggle() {
     
-    if (mobDiv.style.display === 'block' && mobDivIn.style.display === 'block' && hamButton) {
+    if (mobDiv.style.display === 'block' && mobDivIn.style.display === 'none' && hamButton) {
 
         mobDiv.style.display = 'none';
-        mobDivIn.style.display = 'none';
 
     }
     else {
@@ -33,7 +32,7 @@ else {
 }
 
 
-if (window.innerWidth <= 700) {
+if (width <= 700) {
 
 mobileNavClose.addEventListener('click', menuClose);
 function menuClose() {
