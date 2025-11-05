7814f7dfe03b1c451642951f4658d6f12d734def
mastr777 <mstrandberg777@gmail.com>
2025-11-05 17:16:28 +0100

    Added script for Close button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const hamButton = document.querySelector('#hamButton');
 const mobDiv = document.querySelector('.mobileFirstNav');
+const mobDivIn = document.querySelector('.mobileFirstNavIn');
 const mobileNavClose = document.querySelector('#mobileNavClose');
 /* const width = 300;
 const maxWidth = 700; */
@@ -12,28 +13,33 @@ if (window.innerWidth < 700) {
 
 hamButton.addEventListener('click', hamToggle);
 function hamToggle() {
-
-    if (mobDiv.style.display === 'block' && hamButton) {
+    
+    if (mobDiv.style.display === 'block' && mobDivIn.style.display === 'block' && hamButton) {
 
         mobDiv.style.display = 'none';
+        mobDivIn.style.display = 'none';
+
     }
     else {
 
         mobDiv.style.display = 'block';
+        mobDivIn.style.display = 'block';
     }
 }
 }
 else {
     mobDiv.style.display = 'block';
+    mobDivIn.style.display = 'block';
 }
 
 
 if (window.innerWidth <= 700) {
 
-mobileNavClose.addEventListener('click', menyClose);
-function menyClose() {
+mobileNavClose.addEventListener('click', menuClose);
+function menuClose() {
 
     mobDiv.style.display = 'none'; 
+    mobDivIn.style.display = 'none';
     window.location.reload();
 }
 }
