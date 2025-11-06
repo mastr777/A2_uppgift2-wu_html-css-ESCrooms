2eb806ad3da6fdc49268fa367687bbc55628462d
mastr777 <mstrandberg777@gmail.com>
2025-11-06 11:55:28 +0100

    Small formatting

--- script.js
+++ script.js
@@ -11,20 +11,20 @@ const width = window.innerWidth;
 
 if (width < 700) {
 
-hamButton.addEventListener('click', hamToggle);
-function hamToggle() {
-    
-    if (mobDiv.style.display === 'block' && mobDivIn.style.display === 'none' && hamButton) {
+    hamButton.addEventListener('click', hamToggle);
+    function hamToggle() {
 
-        mobDiv.style.display = 'none';
+        if (mobDiv.style.display === 'block' && mobDivIn.style.display === 'none' && hamButton) {
 
+            mobDiv.style.display = 'none';
+
+        }
+        else {
+
+            mobDiv.style.display = 'block';
+            mobDivIn.style.display = 'block';
+        }
     }
-    else {
-
-        mobDiv.style.display = 'block';
-        mobDivIn.style.display = 'block';
-    }
-}
 }
 else {
     mobDiv.style.display = 'block';
@@ -34,11 +34,11 @@ else {
 
 if (width <= 700) {
 
-mobileNavClose.addEventListener('click', menuClose);
-function menuClose() {
+    mobileNavClose.addEventListener('click', menuClose);
+    function menuClose() {
 
-    mobDiv.style.display = 'none'; 
-    mobDivIn.style.display = 'none';
-    window.location.reload();
-}
+        mobDiv.style.display = 'none';
+        mobDivIn.style.display = 'none';
+        window.location.reload();
+    }
 }
